be952f571e10c186700b9ec0cdfb7c1b9143a1ae
David Scott <dave.scott@docker.com>
2020-01-26 10:50:17 +0000

    ftruncate causes the problem
    Signed-off-by: David Scott <dave.scott@docker.com>

--- bad.c
+++ bad.c
@@ -5,70 +5,39 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
-void do_echo() {
-    int fd = openat(AT_FDCWD, "test1.txt", O_WRONLY|O_CREAT|O_TRUNC, 0666);
+void test(const char *dir){
+    if (chdir(dir) == -1) {
+        perror("chdir");
+    };
+    int fd = open("test1.txt", O_WRONLY|O_CREAT, 0666);
     if (fd == -1) {
         perror("openat");
     }
-    int n = write(fd, "Testing\n", 8);
-    if (n != 8) {
-        perror("write");
-    }
-    if (close(fd) == -1){
-        perror("close");
-    }
-}
-
-void do_cat(){
-    int fd = openat(AT_FDCWD, "test1.txt", O_RDONLY);
-    if (fd == -1) {
-        perror("openat");
+    // The ftruncate causes the following fstat to return 0 mtime.
+    if (ftruncate(fd, 0) == -1) {
+        perror("ftruncate");
     }
     struct stat buf;
     if (fstat(fd, &buf) == -1) {
         perror("fstat");
     }
-    char bytes[16];
-    int n = read(fd, &bytes, sizeof(bytes));
-    if (n != 8) {
-        perror("read");
-    }
-    bytes[n] = 0;
-    if (strcmp(&bytes[0], "Testing\n") != 0) {
-        fprintf(stderr, "read %s\n", &bytes[0]);
-        exit(1);
-    }
     if (close(fd) == -1){
         perror("close");
     }
-}
-
-int do_stat(){
-    struct stat buf;
-    if (lstat("test1.txt", &buf) == -1) {
-        perror("lstat");
-    }
-    printf("stat.st_mtime = %ld\n", buf.st_mtime);
+    printf("  stat.st_mtime = %ld\n", buf.st_mtime);
     if (buf.st_mtime == 0) {
-        return 1;
+        printf("  Test failed.\n");
+        return;
     }
-    return 0;
+    printf("  Test succeeded.\n");
 }
 
 int main(int argc, char *argv[]) {
     printf("I expect a volume to be mounted on /mnt.\n");
-    if (chdir("/mnt") == -1) {
-        perror("chdir");
-    };
-    do_echo();
-    do_cat();
-    do_cat();
-    int result = do_stat();
-    if (result == 1) {
-        printf("Test failed.\n");
-        return 1;
-    }
-    printf("Test succeeded.\n");
-    return 0;
+    printf("in / (regular filesystem):\n");
+    test("/");
+    printf("in /mnt (shared volume):\n");
+    test("/mnt");
 }
